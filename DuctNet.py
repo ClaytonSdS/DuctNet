313623d95a8ca6db7ce6ef4325d0c6d5eef76440
Clayton Silva dos Santos <clayton_box@outlook.com>
2022-03-11 13:51:05 -0300

    update 11/03/2022

--- DuctNet.py
+++ DuctNet.py
@@ -37,15 +37,95 @@ class Node():
 
 
 class Net():
-    def __init__(self, no, lk):
-        self.no = no
-        self.lk = lk
-        self.Set_Net()
-        
-    def Set_Net(self):
-        self.net = np.empty((len(self.no), len(self.no)))
-        self.net[:] = np.nan
+    def __init__(self, connectivity, ligacoes, nos):
+        self.ligacoes = ligacoes
+        self.nos = nos
 
-    def Set_Mass_Balance(self):
-        print('oi')
+        self.link = connectivity[0]
+        self.from_ = connectivity[1]
+        self.to_ = connectivity[2]
+        self.Set_Connection()
+        self.mass_balance = {}
+        self.matrix_a = {}
+        self.matrix_b = {}
+
+        for node in range(len(self.nodes)):
+            self.find_links_nodes(self.nodes[node])
+
+        self.Set_Nodes_Neighbors()
+
+        for y in range(len(self.node_variable)):
+            self.Set_Mass_Balance_Equations(self.node_variable[y])
+
+        for z in range(len(self.node_variable)):
+            self.matrix_a[self.node_variable[z]] = self.find_equation_by_node(self.node_variable[z])
+            self.matrix_b[self.node_variable[z]] = self.find_equation_by_node_B(self.node_variable[z])
+
+    def Set_Connection(self):
+        self.connection = pd.DataFrame({"link":self.link,"from":self.from_, "to":self.to_})
+        #self.matrix_A_shape = len(list(set(from_).intersection(self.to_)))
+        self.node_variable = list(set(self.from_).intersection(self.to_))
+        self.node_boundary = list(set(self.from_).symmetric_difference(set(self.to_)))
+        self.nodes = list(self.node_variable + self.node_boundary)
+        self.nodes.sort()
+        self.nodes_links = {self.nodes[i]: [] for i in range(0, len(self.nodes))}
+        self.links_nodes = {f"L_{self.link[i]}": [self.from_[i], self.to_[i]] for i in range(len(self.link))}
+        self.nodes_near = {}
+
+    def find_links_nodes(self, node_to_search):
+        for x in range(len(self.to_)):
+            if self.from_[x] == node_to_search or self.to_[x] == node_to_search:
+                self.nodes_links[node_to_search].append(f"L_{self.link[x]}")
+
+    def Set_Mass_Balance_Equations(self, node):
+        self.mass_balance[node] = {node: [f"a_{self.nodes_links[node][i].split('_')[-1]}" for i in range(len(self.nodes_links[node]))]}
+        for x in range(len(self.nodes_near[node])):
+            self.mass_balance[node][self.nodes_near[node][x]] = f"a_{self.nodes_links[node][x].split('_')[-1]}"
+
+    def Set_Nodes_Neighbors(self):
+        self.neighbors_by_link = {}
+        for x in range(len(self.node_variable)):
+            self.neighbors_by_link[self.node_variable[x]] = []
+        for x in range(len(self.node_variable)):
+            for j in range(len(self.nodes_links[self.node_variable[x]])):
+                self.nodes_links[self.node_variable[x]][j]
+                self.neighbors_by_link[self.node_variable[x]].append(self.nodes_links[self.node_variable[x]][j])
+        for x in range(len(self.node_variable)):
+            for i in range(len(self.neighbors_by_link[self.node_variable[x]])):
+                self.neighbors_by_link[self.node_variable[x]][i] = self.links_nodes[self.neighbors_by_link[self.node_variable[x]][i]]
+        for x in range(len(self.node_variable)):
+            self.nodes_near[self.node_variable[x]] = []
+            for i in range(len(self.neighbors_by_link[self.node_variable[x]])):
+                for z in range(len(self.neighbors_by_link[self.node_variable[x]][i])):
+                    if self.neighbors_by_link[self.node_variable[x]][i][z] != self.node_variable[x]:
+                        self.nodes_near[self.node_variable[x]].append(self.neighbors_by_link[self.node_variable[x]][i][z])
+
+    def find_equation_by_node(self,no):
+        encontrados = []
+        for x in range(len(self.node_variable)):
+            if no in self.mass_balance[self.node_variable[x]].keys():
+                encontrados.append(self.mass_balance[self.node_variable[x]][no])
+            else:
+                encontrados.append(0)
+        return encontrados
+
+    def find_equation_by_node_B(self, no):
+        encontrados = []
+        nos_boundary = self.node_boundary
+        nos = list(self.mass_balance[no].keys())
+        for x in range(len(nos)):
+            if nos[x] in nos_boundary:
+                encontrados.append(self.mass_balance[no][nos[x]] + f'p{nos[x]}')
+
+        if len(encontrados) == 0:
+            encontrados = [0]
+
+        return encontrados
+
+    # m_line = {0: a0(p0) - a0(p1); 1: a1(p1) - a1(p2); 2: a2(p2 + p5) - a2(p3); 3: a3(p3) - a3(p4); 5:a5(p5) - a5(p2)
+    # {2: (a1 + a5) - a3}
+    # preciso saber o que entra e o que sai do nó
+    # m_entra - m_sai = 0
+    # if to -> chegando
+    # if from -> saindo
 
