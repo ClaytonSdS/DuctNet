5618130819deed1f7df46301c6e888b12f11388a
Clayton Silva dos Santos <clayton_box@outlook.com>
2022-03-12 12:13:15 -0300

    updated

--- DuctNet.py
+++ DuctNet.py
@@ -44,39 +44,13 @@ class Net():
         self.dflink = pd.read_csv(flink, sep='\t', index_col='idx')
         self.connect = pd.read_csv(fconnect, sep='\t', index_col='lk')
         self.DictLink()
-        
-        self.ligacoes = ligacoes
-        self.nos = nos
 
-        self.link = connectivity[0]
-        self.from_ = connectivity[1]
-        self.to_ = connectivity[2]
-        self.Set_Connection()
+        #self.Set_Connection()
 
         self.mass_balance = {}
         self.matrix_a = {}
         self.matrix_b = {}
         self.result = {}
-
-        # checar
-        for node in range(len(self.nodes)):
-            self.find_links_nodes(self.nodes[node])
-
-        self.Set_Nodes_Neighbors()
-
-        for y in range(len(self.node_variable)):
-            self.Set_Mass_Balance_Equations(self.node_variable[y])
-
-        for z in range(len(self.node_variable)):
-            self.matrix_a[self.node_variable[z]] = self.Set_Matrix_A_Coefs(self.node_variable[z])
-            self.matrix_b[self.node_variable[z]] = self.Set_Matrix_B_Coefs(self.node_variable[z])
-
-        self.A = np.array(list(self.matrix_a.values()))
-        self.B = np.array(list(self.matrix_b.values()))
-        self.C = np.linalg.solve(self.A, self.B)
-
-        for j in range(len(self.node_variable)):
-            self.result[self.node_variable[j]] = self.C[j]
 
     def DictLink(self):
         self.link = {ID: Link(self.dflink.loc[ID, 'm'],
@@ -88,73 +62,17 @@ class Net():
                      for ID in self.dflink.index}
 
     def Set_Connection(self):
-        self.connection = pd.DataFrame({"link": self.link, "from": self.from_, "to": self.to_})
+        pass
+        #self.node_variable = list(set(self.from_).intersection(self.to_))
+        #self.node_boundary = list(set(self.from_).symmetric_difference(set(self.to_)))
 
-
-        self.node_variable = list(set(self.from_).intersection(self.to_))
-        self.node_boundary = list(set(self.from_).symmetric_difference(set(self.to_)))
-
-        self.nodes = list(self.node_variable + self.node_boundary)
-        self.nodes.sort()
+        #self.nodes = list(self.node_variable + self.node_boundary)
+        #self.nodes.sort()
 
         # checar
-        self.nodes_links = {self.nodes[i]: [] for i in range(0, len(self.nodes))}
-        self.links_nodes = {f"L_{self.link[i]}": [self.from_[i], self.to_[i]] for i in range(len(self.link))}
-        self.nodes_near = {}
-
-    def find_links_nodes(self, node_to_search):
-        for x in range(len(self.to_)):
-            if self.from_[x] == node_to_search or self.to_[x] == node_to_search:
-                self.nodes_links[node_to_search].append(f"L_{self.link[x]}")
-
-    def Set_Mass_Balance_Equations(self, node):
-        self.mass_balance[node] = {node: [- self.ligacoes[int(self.nodes_links[node][i].split('_')[-1])].a for i in
-                                          range(len(self.nodes_links[node]))]}
-        self.mass_balance[node][node] = sum(self.mass_balance[node][node])
-        for x in range(len(self.nodes_near[node])):
-            self.mass_balance[node][self.nodes_near[node][x]] = self.ligacoes[
-                int(self.nodes_links[node][x].split('_')[-1])].a
-
-    def Set_Nodes_Neighbors(self):
-        self.neighbors_by_link = {}
-        for x in range(len(self.node_variable)):
-            self.neighbors_by_link[self.node_variable[x]] = []
-        for x in range(len(self.node_variable)):
-            for j in range(len(self.nodes_links[self.node_variable[x]])):
-                self.nodes_links[self.node_variable[x]][j]
-                self.neighbors_by_link[self.node_variable[x]].append(self.nodes_links[self.node_variable[x]][j])
-        for x in range(len(self.node_variable)):
-            for i in range(len(self.neighbors_by_link[self.node_variable[x]])):
-                self.neighbors_by_link[self.node_variable[x]][i] = self.links_nodes[
-                    self.neighbors_by_link[self.node_variable[x]][i]]
-        for x in range(len(self.node_variable)):
-            self.nodes_near[self.node_variable[x]] = []
-            for i in range(len(self.neighbors_by_link[self.node_variable[x]])):
-                for z in range(len(self.neighbors_by_link[self.node_variable[x]][i])):
-                    if self.neighbors_by_link[self.node_variable[x]][i][z] != self.node_variable[x]:
-                        self.nodes_near[self.node_variable[x]].append(
-                            self.neighbors_by_link[self.node_variable[x]][i][z])
-
-    def Set_Matrix_A_Coefs(self, no):
-        encontrados = []
-        for x in range(len(self.node_variable)):
-            if no in self.mass_balance[self.node_variable[x]].keys():
-                encontrados.append(self.mass_balance[self.node_variable[x]][no])
-            else:
-                encontrados.append(0)
-        return encontrados
-
-    def Set_Matrix_B_Coefs(self, no):
-        encontrados = []
-        nos_boundary = self.node_boundary
-        nos = list(self.mass_balance[no].keys())
-        for x in range(len(nos)):
-            if nos[x] in nos_boundary:
-                encontrados.append(self.mass_balance[no][nos[x]] * self.nos[int(nos[x])].p)
-        if len(encontrados) == 0:
-            encontrados = [0]
-        return encontrados
-
+        #self.nodes_links = {self.nodes[i]: [] for i in range(0, len(self.nodes))}
+        #self.links_nodes = {f"L_{self.link[i]}": [self.from_[i], self.to_[i]] for i in range(len(self.link))}
+        #self.nodes_near = {}
 
     # m_line = {0: a0(p0) - a0(p1); 1: a1(p1) - a1(p2); 2: a2(p2 + p5) - a2(p3); 3: a3(p3) - a3(p4); 5:a5(p5) - a5(p2)
     # {2: (a1 + a5) - a3}
--- tuto/model_0.py
+++ tuto/model_0.py
@@ -5,67 +5,11 @@ import numpy as np
 from DuctNet import DuctNet as dn
 reload(dn)
 
-lk = [dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.5)]
-lk += [dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.1)]
-lk += [dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.3)]
-lk += [dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7)]
+file_path = "C:\\Users\\clayt\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\DuctNet\data"
 
-no = [dn.Node(5000)]
-no += [dn.Node(3400)]
-no += [dn.Node(3150)]
-no += [dn.Node(2170)]
-no += [dn.Node(10)]
-
-
-
-lk = {0: dn.Link(5, 0.002, 0.002, 0.002, 1000, 0.5),
-      1:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.1),
-      2:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.3),
-      3:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      5: dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7)}
-
-no = {0: dn.Node(5000),
-      1: dn.Node(3400),
-      2: dn.Node(3150),
-      3: dn.Node(2170),
-      4: dn.Node(10)}
-
-lk = {0: dn.Link(5, 0.002, 0.002, 0.002, 1000, 0.5),
-      1:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.1),
-      2:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.3),
-      3:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      4:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      5: dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      6:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      7:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      8:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      9:dn.Link(5, 0.002, 0.002,  0.002, 1000, 0.7),
-      }
-
-no = {0: dn.Node(5000),
-      1: dn.Node(3400),
-      2: dn.Node(3150),
-      3: dn.Node(2170),
-      4: dn.Node(10),
-      5: dn.Node(10),
-      6: dn.Node(10),
-      7: dn.Node(10),
-      8: dn.Node(10),
-      9: dn.Node(10),
-      10: dn.Node(10)}
-
-# caso 1 - #######################################################
-net = dn.Net(connectivity= np.array([[0,1,2,3,5],
-                                   [0,1,2,3,5],
-                                   [1,2,3,4,2]]), ligacoes=lk, nos=no)
-# caso 2 - #######################################################
-net = dn.Net(connectivity= np.array([[0,1,2,3,4,5,6,7,8,9],
-                                   [0,1,2,3,5,6,7,8,9,10],
-                                   [1,2,3,4,1,2,3,1,2,3]]), ligacoes=lk, nos=no)
-#caso 3 serie - ##################################################
-net = dn.Net(connectivity= np.array([[0,1,2,3],
-                                   [0,1,2,3],
-                                   [1,2,3,4]]), ligacoes=lk, nos=no)
+net = dn.Net(f'{file_path}\\node.inp',
+            f'{file_path}\\link.inp',
+             f'{file_path}\\connect.inp')
 
 
 
