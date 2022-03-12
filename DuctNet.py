973c42081d27354c4189d8cda0cafc248c1660d5
Clayton Silva dos Santos <clayton_box@outlook.com>
2022-03-12 11:48:11 -0300

    add new functions

--- DuctNet.py
+++ DuctNet.py
@@ -23,6 +23,7 @@ class Link():
         self.zeta = zeta
         self.Set_a()
 
+
     def Set_Dynamic(self):
         self.U = self.m / (self.A_r * self.rho)
         self.p_d = self.rho * self.U ** 2 / 2
@@ -38,7 +39,12 @@ class Node():
 
 
 class Net():
-    def __init__(self, connectivity, ligacoes, nos):
+    def __init__(self, fnode, flink, fconnect):
+        self.node = pd.read_csv(fnode, sep='\t', index_col='idx')
+        self.dflink = pd.read_csv(flink, sep='\t', index_col='idx')
+        self.connect = pd.read_csv(fconnect, sep='\t', index_col='lk')
+        self.DictLink()
+        
         self.ligacoes = ligacoes
         self.nos = nos
 
@@ -71,6 +77,15 @@ class Net():
 
         for j in range(len(self.node_variable)):
             self.result[self.node_variable[j]] = self.C[j]
+
+    def DictLink(self):
+        self.link = {ID: Link(self.dflink.loc[ID, 'm'],
+                              self.dflink.loc[ID, 'A_r'],
+                              self.dflink.loc[ID, 'A_i'],
+                              self.dflink.loc[ID, 'A_o'],
+                              self.dflink.loc[ID, 'rho'],
+                              self.dflink.loc[ID, 'zeta'])
+                     for ID in self.dflink.index}
 
     def Set_Connection(self):
         self.connection = pd.DataFrame({"link": self.link, "from": self.from_, "to": self.to_})
@@ -141,8 +156,6 @@ class Net():
         return encontrados
 
 
-    def printer(self):
-        print(222)
     # m_line = {0: a0(p0) - a0(p1); 1: a1(p1) - a1(p2); 2: a2(p2 + p5) - a2(p3); 3: a3(p3) - a3(p4); 5:a5(p5) - a5(p2)
     # {2: (a1 + a5) - a3}
     # preciso saber o que entra e o que sai do nó
