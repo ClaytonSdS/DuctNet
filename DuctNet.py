2dda0b6e1a426a68ea02ee3401be57db22d1f648
Clayton Silva dos Santos <clayton_box@outlook.com>
2022-03-12 22:21:00 -0300

    new functions

--- DuctNet.py
+++ DuctNet.py
@@ -46,10 +46,10 @@ class Net():
         self.DictLink()
         self.Set_Boundary_Variable_Nodes()
 
-        self.mass_balance = {}
         self.matrix_a = {}
         self.matrix_b = {}
         self.result = {}
+        self.Set_Equations_by_Node()
 
         # COLOCAR OS NOS ASSOCIADOS AS LIGACOES
         self.nodes_in_links = {self.connect.index[node]:  list(self.connect.loc[self.connect.index[node]]) for node in range(len(self.connect.index))}
@@ -58,7 +58,8 @@ class Net():
         self.links_in_nodes = {self.node_variable[node]: self.Set_from_to(self.node_variable[node]) for node  in range(len(self.node_variable))}
 
         # ATIVAR FUNÇÃO PARA ENCONTRAR OS VIZINHOS DOS NÓS VARIAVEIS
-        self.Set_Neighbors()
+        #self.Set_Neighbors()
+        self.Set_mass_balance()
 
     def DictLink(self):
         self.link = {ID: Link(self.dflink.loc[ID, 'm'],
@@ -70,10 +71,7 @@ class Net():
                      for ID in self.dflink.index}
 
     def Set_from_to(self, no):
-        saindo = self.connect.loc[self.connect['from']==no].index
-        entrando = self.connect.loc[self.connect['to']==no].index
-        total = list(saindo) + list(entrando)
-        return total
+        return {'in': list(self.connect.loc[self.connect['to']==no].index), 'out': list(self.connect.loc[self.connect['from']==no].index)}
 
     def Set_Boundary_Variable_Nodes(self):
         self.node_variable = list(set(self.connect['from'].values).intersection(self.connect['to'].values))
@@ -84,6 +82,16 @@ class Net():
             if lista[x] != no:
                 return lista[x]
 
+    def Set_mass_balance(self):
+        nodes = self.node_variable + self.node_boundary
+        nodes.sort()
+
+        self.mass_balance = pd.DataFrame({"node": nodes}).set_index("node")
+        #in_ = sum([net.equations[net.links_in_nodes[2]["in"][i]].values for i in range(len(net.links_in_nodes[2]["in"]))])
+        #out_ = sum([net.equations[net.links_in_nodes[2]["out"][i]].values for i in range(len(net.links_in_nodes[2]["out"]))])
+        for node in range(len(self.node_variable)):
+            self.mass_balance[self.node_variable[node]] = sum([self.equations[self.links_in_nodes[self.node_variable[node]]["in"][i]].values for i in range(len(self.links_in_nodes[self.node_variable[node]]["in"]))]) - sum([self.equations[self.links_in_nodes[self.node_variable[node]]["out"][i]].values for i in range(len(self.links_in_nodes[self.node_variable[node]]["out"]))])
+
     def Set_Neighbors(self):
         self.nodes_neighbors = {}
         for no in range(len(self.node_variable)):
@@ -91,7 +99,19 @@ class Net():
             for link in range(len(self.links_in_nodes[self.node_variable[no]])):
                 self.nodes_neighbors[self.node_variable[no]].append(self.find_different_node(self.nodes_in_links[self.links_in_nodes[self.node_variable[no]][link]], self.node_variable[no]))
 
+    def Set_Equations_by_Node(self):
+        nodes = self.node_variable+self.node_boundary
+        nodes.sort()
+        links = {self.connect.index[i]:[] for i in range(len(self.connect.index))}
+        self.equations = pd.DataFrame({"node": nodes}).set_index("node")
+        for link in range(len(self.connect.index)):
+            self.equations[self.connect.index[link]] = np.zeros(len(nodes))
 
+        for link in range(len(self.connect.index.values)):
+            self.equations[self.connect.index.values[link]][self.connect.loc[self.connect.index.values[link]].values[0]] = self.link[self.connect.index.values[link]].a
+            self.equations[self.connect.index.values[link]][self.connect.loc[self.connect.index.values[link]].values[1]] = - self.link[self.connect.index.values[link]].a
+
+        #net.connect.loc["d1"]
     # m_line = {0: a0(p0) - a0(p1); 1: a1(p1) - a1(p2); 2: a2(p2 + p5) - a2(p3); 3: a3(p3) - a3(p4); 5:a5(p5) - a5(p2)
     # {2: (a1 + a5) - a3}
     # preciso saber o que entra e o que sai do nó
