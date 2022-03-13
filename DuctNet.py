9082c9dea83427ce720358c48913487fc77fc0ec
Clayton Silva dos Santos <clayton_box@outlook.com>
2022-03-13 15:45:43 -0300

    mass balance error

--- DuctNet.py
+++ DuctNet.py
@@ -40,26 +40,33 @@ class Node():
 
 class Net():
     def __init__(self, fnode, flink, fconnect):
-        self.node = pd.read_csv(fnode, sep='\t', index_col='idx')
-        self.dflink = pd.read_csv(flink, sep='\t', index_col='idx')
-        self.connect = pd.read_csv(fconnect, sep='\t', index_col='lk')
+        self.node = pd.read_csv(fnode, sep='|', index_col='idx')
+        self.dflink = pd.read_csv(flink, sep='|', index_col='idx')
+        self.connect = pd.read_csv(fconnect, sep='|', index_col='lk')
         self.DictLink()
-        self.Set_Boundary_Variable_Nodes()
 
-        self.matrix_a = {}
-        self.matrix_b = {}
-        self.result = {}
-        self.Set_Equations_by_Node()
+        # NÓS DE CONTORNO E NÓS DE BALANÇO DE MASSA (VARIAVEIS)
+        self.node_variable = list(set(self.connect['from'].values).intersection(self.connect['to'].values))
+        self.node_boundary = list(set(self.connect['from'].values).symmetric_difference(set(self.connect['to'].values)))
+
+
+        self.Set_Mass_Equations()
 
         # COLOCAR OS NOS ASSOCIADOS AS LIGACOES
         self.nodes_in_links = {self.connect.index[node]:  list(self.connect.loc[self.connect.index[node]]) for node in range(len(self.connect.index))}
 
         # COLOCAR AS LIGACOES ASSOCIADAS AOS NÓS VARIAVEIS
-        self.links_in_nodes = {self.node_variable[node]: self.Set_from_to(self.node_variable[node]) for node  in range(len(self.node_variable))}
+        self.links_in_nodes = {self.node_variable[node]: self.Set_In_Out(self.node_variable[node]) for node  in range(len(self.node_variable))}
 
         # ATIVAR FUNÇÃO PARA ENCONTRAR OS VIZINHOS DOS NÓS VARIAVEIS
-        #self.Set_Neighbors()
         self.Set_mass_balance()
+
+        # CRIAR AS MATRIX DO SOLVER
+        self.matrix_a = self.mass_balance[:].loc[self.node_variable].values
+        self.Add_Pressure_in_Nodes_Boundary()   # ADICIONAR PRESSAO AOS TERMOS DE CONTORNO
+        self.matrix_b = self.mass_balance[:].loc[self.node_boundary].cumsum().values[-1] * -1
+        self.t = np.linalg.solve(self.matrix_a,self.matrix_b)
+        self.result = {self.node_variable[i]:np.linalg.solve(self.matrix_a,self.matrix_b)[i] for i in range(len(self.node_variable))}
 
     def DictLink(self):
         self.link = {ID: Link(self.dflink.loc[ID, 'm'],
@@ -70,52 +77,45 @@ class Net():
                               self.dflink.loc[ID, 'zeta'])
                      for ID in self.dflink.index}
 
-    def Set_from_to(self, no):
+    # SEPARAR FLUXOS CHEGANDO E SAINDO DE NÓS (IN, OUT)
+    def Set_In_Out(self, no):
+        # se exisir negativo em in -> colocar em out (em modulo)
+        # se existir negativo em out -> colocar em in (em modulo)
+
         return {'in': list(self.connect.loc[self.connect['to']==no].index), 'out': list(self.connect.loc[self.connect['from']==no].index)}
 
-    def Set_Boundary_Variable_Nodes(self):
-        self.node_variable = list(set(self.connect['from'].values).intersection(self.connect['to'].values))
-        self.node_boundary = list(set(self.connect['from'].values).symmetric_difference(set(self.connect['to'].values)))
-
-    def find_different_node(self, lista, no):
-        for x in range(len(lista)):
-            if lista[x] != no:
-                return lista[x]
-
+    # FUNÇÃO PARA CALCULO DOS BALANÇO DE MASSAS NOS NÓS VARIAVEIS
     def Set_mass_balance(self):
         nodes = self.node_variable + self.node_boundary
         nodes.sort()
-
         self.mass_balance = pd.DataFrame({"node": nodes}).set_index("node")
         #in_ = sum([net.equations[net.links_in_nodes[2]["in"][i]].values for i in range(len(net.links_in_nodes[2]["in"]))])
         #out_ = sum([net.equations[net.links_in_nodes[2]["out"][i]].values for i in range(len(net.links_in_nodes[2]["out"]))])
         for node in range(len(self.node_variable)):
-            self.mass_balance[self.node_variable[node]] = sum([self.equations[self.links_in_nodes[self.node_variable[node]]["in"][i]].values for i in range(len(self.links_in_nodes[self.node_variable[node]]["in"]))]) - sum([self.equations[self.links_in_nodes[self.node_variable[node]]["out"][i]].values for i in range(len(self.links_in_nodes[self.node_variable[node]]["out"]))])
+            mass_entering = [self.equations[self.links_in_nodes[self.node_variable[node]]["in"][i]].values for i in range(len(self.links_in_nodes[self.node_variable[node]]["in"]))]
+            mass_leaving =
+            self.mass_balance[self.node_variable[node]] = sum() - sum()
 
-    def Set_Neighbors(self):
-        self.nodes_neighbors = {}
-        for no in range(len(self.node_variable)):
-            self.nodes_neighbors[self.node_variable[no]] = []
-            for link in range(len(self.links_in_nodes[self.node_variable[no]])):
-                self.nodes_neighbors[self.node_variable[no]].append(self.find_different_node(self.nodes_in_links[self.links_in_nodes[self.node_variable[no]][link]], self.node_variable[no]))
+    # ADICIONAR OS TERMOS DE PRESSÃO AOS TERMOS CONSTANTES
+    def Add_Pressure_in_Nodes_Boundary(self):
+        for n_contorno in range(len(self.node_boundary)):
+            self.mass_balance.loc[self.mass_balance.index[self.node_boundary[n_contorno]]] = self.mass_balance.loc[self.mass_balance.index[self.node_boundary[n_contorno]]].values * self.node.loc[self.node.index[self.node_boundary[n_contorno]]].values
 
-    def Set_Equations_by_Node(self):
+    # FUNÇÃO PARA ASSOCIAR OS TERMOS a_n COM AS PRESSOES p_saida E p_from
+    def Set_Mass_Equations(self):
         nodes = self.node_variable+self.node_boundary
         nodes.sort()
         links = {self.connect.index[i]:[] for i in range(len(self.connect.index))}
         self.equations = pd.DataFrame({"node": nodes}).set_index("node")
-        for link in range(len(self.connect.index)):
-            self.equations[self.connect.index[link]] = np.zeros(len(nodes))
 
+        # CRIAR COLUNAS COM OS NOMES DAS LIGAÇÕES E ATRIBUIR UM ARRAY DE ZEROS
+        for link_name in range(len(self.connect.index)):
+            self.equations[self.connect.index[link_name]] = np.zeros(len(nodes))
+
+        # COLOCAR a_n E -a_n NAS LIGAÇÕES DE ACORDO COM AS PRESSOES ENTRADA E SAIDA (SENTIDO DO ESCOAMENTO)
         for link in range(len(self.connect.index.values)):
+            # se exisir negativo em in -> colocar em out (em modulo)
+            # se existir negativo em out -> colocar em in (em modulo)
+
             self.equations[self.connect.index.values[link]][self.connect.loc[self.connect.index.values[link]].values[0]] = self.link[self.connect.index.values[link]].a
             self.equations[self.connect.index.values[link]][self.connect.loc[self.connect.index.values[link]].values[1]] = - self.link[self.connect.index.values[link]].a
-
-        #net.connect.loc["d1"]
-    # m_line = {0: a0(p0) - a0(p1); 1: a1(p1) - a1(p2); 2: a2(p2 + p5) - a2(p3); 3: a3(p3) - a3(p4); 5:a5(p5) - a5(p2)
-    # {2: (a1 + a5) - a3}
-    # preciso saber o que entra e o que sai do nó
-    # m_entra - m_sai = 0
-    # if to -> chegando
-    # if from -> saindo
-
